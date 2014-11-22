402e69e28b4700b84d86f99d8d64a47763596166
Robert Brooks <robertb1@uw.edu>
2014-11-22 15:17:12 -0800

    progress

--- js/app.js
+++ js/app.js
@@ -3,6 +3,21 @@
 
 "use strict";
 
+
+$(document).ready(function() {
+    var mapElem = document.getElementById('map');
+    var infoWindow = new google.maps.InfoWindow();
+
+    var center = {
+        lat: 47.6,
+        lng: -122.3
+    };
+
+    var map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 12
+    });
+});
 //put your code here to create the map, fetch the list of traffic cameras
 //and add them as markers on the map
 //when a user clicks on a marker, you should pan the map so that the marker
